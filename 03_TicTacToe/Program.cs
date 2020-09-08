37ec3c4279e108ac8a3bbba2298c07df98d2e40c
Coste Maxime <maxime.coste2@hotmail.fr>
2020-09-08 20:18:29 +0200

    Can play multiple games and then print stats

--- 03_TicTacToe/Program.cs
+++ 03_TicTacToe/Program.cs
@@ -11,27 +11,54 @@ namespace _03_TicTacToe
         static void Main(string[] args)
         {
             const int boardSize = 3;
-            IPlayer player1 = new WinningMoveIA("Player1", 'X', boardSize);
+            IPlayer player1 = new SmartWinningMoveIA("Player1", 'X', boardSize);
             IPlayer player2 = new WinningMoveIA("Player2", 'O', boardSize);
+
+            repeatPlay(player1, player2, boardSize, 50);
+            Console.ReadKey();
+        }
+
+        static void repeatPlay(IPlayer player1, IPlayer player2, int boardSize, int numberOfGames)
+        {
+            int draws = 0;
+            int player1WinCount = 0;
+            int player2WinCount = 0;
+
+            for (int i = 0; i < numberOfGames; i++)
+            {
+                IPlayer? winner = play(player1, player2, boardSize, false);
+
+                if (winner == null)
+                {
+                    draws++;
+                }
+                else if (winner == player1)
+                {
+                    player1WinCount++;
+                }
+                else
+                {
+                    player2WinCount++;
+                }
+            }
+
+            Console.WriteLine("Draws : " + draws);
+            Console.WriteLine("Player1 wins count : " + player1WinCount);
+            Console.WriteLine("Player2 wins count : " + player2WinCount);
+        }
+
+        static IPlayer? play(IPlayer player1, IPlayer player2, int boardSize, bool render = true)
+        {
             Engine engine = new Engine(player1, player2, boardSize);
-            engine.Render();
+            if (render) { engine.Render(); }
 
             while (!engine.IsGameOver())
             {
                 engine.NextMove();
-                engine.Render();
+                if (render) { engine.Render(); }
             }
 
-            if (engine.Winner == null)
-            {
-                Console.WriteLine("Draw");
-            }
-            else
-            {
-                Console.WriteLine(engine.Winner);
-            }
-
-            Console.ReadKey();
+            return engine.Winner;
         }
     }
 }
